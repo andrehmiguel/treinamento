533a256230798beadbb56f4d3625c1a629de1d6f
André Miguel do Nascimento <andrehmiguel@outlook.com>
2020-12-02 17:12:07 -0300

    Create ex04.py

---
+++ exercicioLista01/ex04.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+print("Entre com as notas de cada bimestre para calcular a média.")
+bim1 = float(input("Insina a nota do 1º bimestre: "))
+bim2 = float(input("Insina a nota do 2º bimestre: "))
+bim3 = float(input("Insina a nota do 3º bimestre: "))
+bim4 = float(input("Insina a nota do 4º bimestre: "))
+media = (bim1 + bim2 + bim3 + bim4) / 4
+print("A média final é ", media,".")
