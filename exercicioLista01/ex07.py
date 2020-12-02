aa41fd748f59a2c8b3250d1afe0180abcbf2dc1f
André Miguel do Nascimento <andrehmiguel@outlook.com>
2020-12-02 17:52:39 -0300

    Create ex07.py

---
+++ exercicioLista01/ex07.py
@@ -0,0 +1,6 @@
+#07 Faça um Programa que peça a altura e calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+l = float(input("Informe a altura do quadrado: "))
+area = l ** 2
+vfinal = area * 2
+print("A área do quadrado é ", area ,"e ", vfinal, "corresponde ao dobro dela.")
