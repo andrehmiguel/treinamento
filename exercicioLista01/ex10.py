d2f2e787f422d163f08d2517da9b6f7e58a305d2
André Miguel do Nascimento <andrehmiguel@outlook.com>
2020-12-02 18:20:01 -0300

    Create ex10.py

---
+++ exercicioLista01/ex10.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit.
+
+
+
+C = int(input("Informe a temperatura em ºC: "))
+F = ((C / 5) * 9) + 32
+print(C, "graus Celsius, equivalem a", F, "graus Fahrenheit.")
