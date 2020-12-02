084e209667887394b6ea97e961944995207a7776
André Miguel do Nascimento <andrehmiguel@outlook.com>
2020-12-02 17:21:36 -0300

    Create ex05.py

---
+++ exercicioLista01/ex05.py
@@ -0,0 +1,5 @@
+#05 Faça um Programa que converta metros para centímetros.
+
+metros = int(input("Insira o número de metros a serem convertidos para centímetros: "))
+centimetros = metros*100
+print(metros , "metros equivalem a", centimetros, "centímetros.")
