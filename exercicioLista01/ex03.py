582bf8d1b56aaca168d0efdcc10e8dcfb8d8b6f2
André Miguel do Nascimento <andrehmiguel@outlook.com>
2020-12-02 17:03:32 -0300

    Create ex03.py

---
+++ exercicioLista01/ex03.py
@@ -0,0 +1,6 @@
+#3 Faça um Programa que peça dois números e imprima a soma.
+
+print("Entre com dois números para serem somados.")
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite outro número: "))
+print("O valor da soma dos dois números é ", numero1 + numero2, ".")
