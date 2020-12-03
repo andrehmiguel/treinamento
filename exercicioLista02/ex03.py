4117ab990638f7aa46445da58a2a1f0577862a74
André Miguel do Nascimento <andrehmiguel@outlook.com>
2020-12-03 10:00:06 -0300

    Create ex03.py

---
+++ exercicioLista02/ex03.py
@@ -0,0 +1,10 @@
+#03 Nome na vertical. Faça um programa que solicite o nome do usuário e imprima-o na vertical.
+
+"""nome = input("Digite seu nome: ")
+i = 0
+while i < len(nome):
+    print(nome[i])
+    i += 1"""
+
+for x in input("Digite seu nome: "):
+   print(x)
