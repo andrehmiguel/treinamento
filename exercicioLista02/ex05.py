1bbd875ec61877870f89546a6ec283681a62df14
André Miguel do Nascimento <andrehmiguel@outlook.com>
2020-12-03 10:29:34 -0300

    Create ex05.py

---
+++ exercicioLista02/ex05.py
@@ -0,0 +1,7 @@
+#05 Nome na vertical em escada invertida. Altere o programa anterior de modo que a escada seja invertida.
+
+nome = input("Digite seu nome: ")
+i = 0
+while i < len(nome):
+    print(nome[i:])
+    i += 1
