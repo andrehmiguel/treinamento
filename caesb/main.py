18d8f20b9d12b471acdd677431391d99b8c164f1
André Miguel do Nascimento <andrehmiguel@outlook.com>
2020-12-07 15:55:15 -0300

    caesb criado

---
+++ caesb/main.py
@@ -0,0 +1,10 @@
+
+
+
+
+consumo = float(input('Informe seu consumo (m³): '))
+unidades = int(input('Informe a quantidade de unidades de consumo: '))
+
+consumo_unidade = consumo / unidades
+
+print(consumo_unidade)
